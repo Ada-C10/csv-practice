f96036d611cf90df39577ea443482a34bd6eab67
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-08-16 21:18:10 -0700

    load data complete

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -2,14 +2,41 @@
 require 'csv'
 require 'awesome_print'
 
+# filename = "../data/athlete_events.csv"
 # Part 1 - CSV Practice
 def load_data(filename)
-
+  olympic_data = CSV.open(filename, 'r', headers: true).map do |item|
+    item.to_h
+  end
+  return olympic_data
+  
 end
+olympic_data = load_data("../data/athlete_events.csv")
+ap olympic_data.class
+
 
 def total_medals_per_country(olympic_data)
-
+  medal_hash = {}
+  medal_tally = 0
+  medals = %w(Gold Silver Bronze)
+  ap medals
+  olympic_data.each do |hash|
+    hash.each do |k,v|
+      # ap k['Medal']
+      if medals.include?(k['Medal'])
+        medal_tally += 1
+        team = k
+        medal_hash = {k => medal_tally}
+      end
+    end
+  end
+  return medal_hash
 end
+# ap olympic_data[0]['Medal']
+medal_hash = total_medals_per_country(olympic_data)
+ap medal_hash.class
+
+
 
 def save_medal_totals(filename, medal_totals)
 
